20613af158a42b7eb704316b2612ce6c23ce2c54
Manthan-Nocode <167435620+Manthan-Nocode@users.noreply.github.com>
2025-03-23 14:45:05 +0000

    test css 14

---
+++ global.d.ts
@@ -0,0 +1,7 @@
+// Type definitions for Google Analytics and Vercel Analytics
+interface Window {
+    // Google Analytics
+    gtag?: (...args: any[]) => void
+    // Vercel Analytics
+    va?: (...args: any[]) => void
+  }  
